a909934f99e809563d2602b5374acbefb446fb63
jannafierst <jfierst@fiu.edu>
2022-12-31 17:41:13 -0500

    Create flower.py

---
+++ LectureScripts/5.turtles/flower.py
@@ -0,0 +1,42 @@
+"""This module contains a code example related to
+Think Python, 2nd Edition
+by Allen Downey
+http://thinkpython2.com
+Copyright 2015 Allen Downey
+License: http://creativecommons.org/licenses/by/4.0/
+"""
+
+from __future__ import print_function, division
+
+from polygon import arc
+
+def petal(t, r, angle):
+    """Draws a petal using two arcs.
+    t: Turtle
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(2):
+        arc(t, r, angle)
+        t.lt(180-angle)
+
+
+def flower(t, n, r, angle):
+    """Draws a flower with n petals.
+    t: Turtle
+    n: number of petals
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(n):
+        petal(t, r, angle)
+        t.lt(360.0/n)
+
+
+def move(t, length):
+    """Move Turtle (t) forward (length) units without leaving a trail.
+    Leaves the pen down.
+    """
+    t.pu()
+    t.fd(length)
+    t.pd()
